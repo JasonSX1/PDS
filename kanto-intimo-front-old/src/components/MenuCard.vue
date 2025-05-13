eec238d0e3c15030af5e6a111a0e502fb72eed98
Geison de Oliveira <geisonyt3@gmail.com>
2025-05-13 17:26:28 -0300

    --

---
+++ kanto-intimo-front-old/src/components/MenuCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="bg-white border rounded-lg px-4 py-6 shadow hover:shadow-md transition cursor-pointer flex items-center justify-start gap-3 min-w-[150px] max-w-full"
+  >
+    <div class="text-3xl flex-shrink-0">{{ icon }}</div>
+    <div class="text-lg font-bold text-gray-800 whitespace-nowrap">{{ label }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  icon: string
+  label: string
+}>()
+</script>
